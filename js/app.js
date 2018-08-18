ccf5da4b5cf7d4707b1e84adc463ac2672e4295b
JennaKoslowski <jenna.koslowski@yahoo.com>
2018-08-18 10:12:12 -0500

    add moves function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
 /*
  * Create a list that holds all of your cards
  */
-let cardImg = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb", "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o",  "fa fa-cube"] ;
+let cardImg = [
+  "fa fa-diamond",
+  "fa fa-paper-plane-o",
+  "fa fa-anchor",
+  "fa fa-bolt",
+  "fa fa-cube",
+  "fa fa-anchor",
+  "fa fa-leaf",
+  "fa fa-bicycle",
+  "fa fa-diamond",
+  "fa fa-bomb",
+  "fa fa-leaf",
+  "fa fa-bomb",
+  "fa fa-bolt",
+  "fa fa-bicycle",
+  "fa fa-paper-plane-o",
+  "fa fa-cube"
+];
 
 let openCard = [];
 
@@ -56,13 +73,7 @@ function match() {
     openCard = [];
   }
 }
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+function moves() {
+  movesCount++;
+  document.querySelector(".moves").innerHTML = movesCount;
+}
