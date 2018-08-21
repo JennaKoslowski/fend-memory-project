7e5c46cadf3f52080f82ce1db48f974ca49d01f2
JennaKoslowski <jenna.koslowski@yahoo.com>
2018-08-21 06:39:43 -0500

    sync with CodePen

--- js/app.js
+++ js/app.js
@@ -21,17 +21,20 @@ let cardImg = [
 ];
 
 let openCard = [];
+let matchedCards = [];
 let cardCat = document.querySelectorAll(".card");
 let deck = document.querySelector(".deck");
 let stars = document.querySelectorAll(".fa-star");
 let movesCount = 0;
+var restart = document.querySelector(".restart");
 let seconds = 0;
 let minutes = 0;
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
   var currentIndex = array.length,
-      temporaryValue,
-      randomIndex;
+    temporaryValue,
+    randomIndex;
 
   while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
@@ -48,10 +51,10 @@ function shuffleCards() {
   let deckChild = Array.from(document.querySelectorAll(".deck li"));
   let shuffledCards = shuffle(deckChild);
   for (var cardType of shuffledCards) {
-    console.log(deck);
-   deck.appendChild(cardType);
-} }
-shuffleCards(); 
+    deck.appendChild(cardType);
+  }
+}
+shuffleCards();
 
 let event = function(event) {
   if (event.target.className === "card" && openCard.length < 2) {
@@ -61,6 +64,7 @@ let event = function(event) {
       setTimeout(match, 1000);
       moves();
       starCount();
+      setTimeout(timeCount, 1000);
     }
   }
 };
@@ -70,13 +74,17 @@ for (var card of cardCat) {
   card.addEventListener("click", event);
 }
 
-function match() { //check if selected cards match
+function match() {
+  //check if selected cards match
   let cardA = openCard[0].firstElementChild.className;
   let cardB = openCard[1].firstElementChild.className;
   if (cardA === cardB) {
     $(openCard[0]).toggleClass("match");
     $(openCard[1]).toggleClass("match");
-    openCard = []; 
+    matchedCards.push(cardA);
+    matchedCards.push(cardB);
+    console.log(matchedCards);
+    openCard = [];
   } else {
     $(openCard[0]).removeClass("open show");
     $(openCard[1]).removeClass("open show");
@@ -93,12 +101,17 @@ function starCount() {
   for (var star of stars) {
     if (movesCount === 10 || movesCount === 20 || moves === 30) {
       if (star.style.display !== "none") {
-        star.style.display = "none";  
+        star.style.display = "none";
         break;
       }
     }
+    else if (star.style.display = "none") {
+      star.style.display = "inline-block";
+    }
   }
 }
+setInterval(timeCount, 500);
+
 function timeCount() {
   seconds++;
   if (seconds <10) {
@@ -110,6 +123,7 @@ function timeCount() {
     document.querySelector(".stopwatch").innerHTML = minutes + " :" + seconds;
   }
 }
+
 
 function endGame() {
   if (matchedCards == 4){
